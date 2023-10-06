fc2fa381c38be130fb7a8e3db5b1a8525e6425a5
hghanbari <gh.hamid89@gmail.com>
2023-10-06 12:41:48 +0200

    first commit

---
+++ public/staff/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+  <title>GBI</title>
+  <meta charset="utf-8">
+</head>
+<header>
+  <h1>GBI staff Area</h1>
+</header>
+<nav>
+  <ul>
+    <li><a href="index.php">Menu</a></li>
+  </ul>
+</nav>
+<footer>
+  &copy; <?php echo date('Y') ?> Globe Bank
+</footer>
+
+<body>
+
+</body>
+
+</html>
