499b784146114f6e91517d14764944e84b3d990c
hghanbari <gh.hamid89@gmail.com>
2023-11-06 10:46:43 +0100

    test 2

--- public/staff/subjects/index.php
+++ public/staff/subjects/index.php
@@ -1,7 +1,7 @@
 <?php require_once('../../../private/initialize.php'); ?>
 <?php $page_title = 'Subjects'; ?>
-<?php include('../../../private/shared/staff_header.php'); ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
 
 <div id="content"></div>
 
-<?php include('../../../private/shared/staff_footer.php'); ?>
+<?php include(SHARED_PATH . '/staff_footer.php'); ?>
