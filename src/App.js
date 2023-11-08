cbe8b593260bcacabffccffe7a4c627bdc1d02c5
hghanbari <gh.hamid89@gmail.com>
2023-11-08 14:02:46 +0100

    the first the format of web

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import MenuAppBar from "./header";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import PermanentDrawerLeft from "./sidebar";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
-  );
+  return <PermanentDrawerLeft />;
 }
 
 export default App;
