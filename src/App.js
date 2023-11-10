437a724df7c068dd1c65f3255cf6c6ea418227f7
hghanbari <gh.hamid89@gmail.com>
2023-11-10 22:45:35 +0100

    style footer header sidebar fist step

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
-import MenuAppBar from "./header";
-import CssBaseline from "@mui/material/CssBaseline";
-import Box from "@mui/material/Box";
-import PermanentDrawerLeft from "./sidebar";
+import Header from "./header";
+import Sidebar from "./sidebar";
+import Footer from "./footer";
+import "./styles/main.css";
+
 function App() {
-  return <PermanentDrawerLeft />;
+  return (
+    <main className="app">
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+      />
+      <Sidebar />
+      <div className="app-body">
+        <Header />
+        <div className="body-content">
+          <h2 className="main-title">salam</h2>
+
+          <div>
+            <div className="circle">
+              <span class="material-symbols-outlined">home</span>
+            </div>
+          </div>
+
+          <h2>salam</h2>
+          <h2>salam</h2>
+          <h2>salam</h2>
+          <h2>salam</h2>
+          <h2>salam</h2>
+          <h2>salam</h2>
+          <h2>salam</h2>
+          <h2>salam</h2>
+          <h2>salam</h2>
+          <h2>salam</h2>
+          <h2>salam</h2>
+          <h2>salam</h2>
+          <h2>salam</h2>
+          <h2>salam</h2>
+          <h2>salam</h2>
+          <h2>salam</h2>
+          <h2>salam</h2>
+          <h2>salam</h2>
+        </div>
+        <Footer />
+      </div>
+    </main>
+  );
 }
 
 export default App;
