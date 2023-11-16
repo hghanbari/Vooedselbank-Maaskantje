d286ba91691cc5ad80918ef4168e1201b616fb1f
Hero Hoekstra <2172289@talnet.nl>
2023-11-16 10:56:39 +0100

    Small correction to add supplier
    Now it checks for user auth and uses the new CheckIfExists function

--- public/php/actions/add/supplier.php
+++ public/php/actions/add/supplier.php
@@ -4,15 +4,19 @@ include_once("../../functions.php");
 $conn = ConnectDB("root", "");
 
 try {
-    // Check if supplier already exits
-    $query = $conn->prepare("SELECT companyName FROM supplier");
-    $query->execute();
-
-    $result = $query->fetchAll();
-
-    if(!empty($result)) {
+    // Check auth
+    if (!CheckAuth(3, $conn)) {
         // Create error cookie
+        header('Location: ' . $_SERVER['HTTP_REFERER']);
+        exit();
+    }
 
+    // Check if supplier already exits
+    $stmt = 'SELECT `companyName` FROM `supplier` WHERE `companyName` = :name';
+    $data = [':name' => $_POST['bedrijfsnaam']];
+
+    if(CheckIfExists($stmt, $data, $conn)) {
+        // Create error cookie
         header('Location: ' . $_SERVER['HTTP_REFERER']);
         exit();
     }
