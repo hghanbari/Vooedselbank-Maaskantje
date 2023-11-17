c9d9fd8fe60105037df0af1a05b88fa926f32aec
hghanbari <gh.hamid89@gmail.com>
2023-11-17 22:04:44 +0100

    log in page klaar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import Header from "./header";
 import Sidebar from "./sidebar";
 import Footer from "./footer";
+import Login from "./login";
 import "./styles/main.css";
 
 function App() {
+  fetch("php/account/login.php", {
+    method: "POST",
+    body: JSON.stringify({
+      email: "",
+      password: "",
+    }),
+  });
   return (
-    <main className="app">
+    <main>
       <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
       />
-      <Sidebar />
-      <div className="app-body">
-        <Header />
-        <div className="body-content"></div>
-        <Footer />
-      </div>
+      <Login />
     </main>
   );
 }
---
+++ src/login.js
@@ -0,0 +1,52 @@
+import * as React from "react";
+import "./styles/login.css";
+
+export default function Login() {
+  return (
+    <>
+      <div className="backdrop">
+        <span></span>
+        <span></span>
+      </div>
+      <header className="login-header">
+        <h1 className="header-title">Welkom!</h1>
+        <p className="header-Paragraph">
+          Log in met uw gebruikersnaam en wachtwoord.
+        </p>
+      </header>
+      <div className="card">
+        <div id="card-content">
+          <div id="card-title">
+            <h2>LOGIN</h2>
+            <div class="underline-title"></div>
+          </div>
+          <form method="post" class="form">
+            <label for="user-email">&nbsp;Email</label>
+            <input
+              id="user-email"
+              class="form-content"
+              type="email"
+              name="email"
+              autocomplete="on"
+              required
+            />
+            <div class="form-border"></div>
+            <label for="user-password">&nbsp;Password</label>
+            <input
+              id="user-password"
+              class="form-content"
+              type="password"
+              name="password"
+              required
+            />
+            <div class="form-border"></div>
+            <a href="#">
+              <legend className="forgot-pass">Forgot password?</legend>
+            </a>
+            <input id="submit-btn" type="submit" name="submit" value="LOGIN" />
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
