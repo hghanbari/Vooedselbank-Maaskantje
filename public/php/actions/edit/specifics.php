0fceccd79a12917e35b98c472ed97dd76f04dd5d
Hero Hoekstra <2172289@talnet.nl>
2023-11-23 17:38:55 +0100

    edit specs

---
+++ public/php/actions/edit/specifics.php
@@ -0,0 +1,25 @@
+<?php
+include_once('../../functions.php');
+
+$conn = ConnectDB('root', '');
+
+try {
+    // Check auth
+    if (!CheckAuth(2, $conn)) {
+        header('Location: ' . $_SERVER['HTTP_REFERER']);
+        exit();
+    }
+
+    // Update if changed
+    if ($_POST['desc'] != '') {
+        $data = [
+            ':id' => $_POST['id'],
+            ':desc' => $_POST['desc']
+        ];
+
+        $query = $conn->prepare('UPDATE `specifics` SET `desc` = :desc WHERE `specificId` = :id');
+        $query->execute($data);
+    }
+} catch (PDOException $e) {
+    echo "Error!: " . $e->getMessage();
+}
