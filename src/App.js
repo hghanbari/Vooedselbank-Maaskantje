736001ef60beb3a5d5c3a677bc9d7bd992abbf66
Mansoor <mansoor1999@live.com>
2023-11-27 10:38:53 +0100

    push 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import Header from "./header";
 import Sidebar from "./sidebar";
 import Footer from "./footer";
-import Login from "./login";
+import Customers from "./pages/customers";
 import "./styles/main.css";
 
 function App() {
-  fetch("php/account/login.php", {
-    method: "POST",
-    body: JSON.stringify({
-      email: "",
-      password: "",
-    }),
-  });
   return (
-    <main>
+    <main className="app">
       <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
       />
-      <Login />
+      <Sidebar />
+      <div className="app-body">
+        <Header />
+     
+     <Customers/>
+        <Footer />
+        
+      </div>
     </main>
   );
 }
