fc4c2da65b9b7d853a6d16d14211991dba30f580
kenny meijer <kmeijer@roc-dev.com>
2023-12-11 10:19:03 +0100

    backend update

--- public/php/actions/add/product.php
+++ public/php/actions/add/product.php
@@ -4,6 +4,13 @@ include_once('../../functions.php');
 // Connect DB
 $conn = ConnectDB("root", "");
 
+header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Credentials: true');
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST, DELETE, OPTIONS");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 try {
     if (!CheckAuth(2, $conn)) {
         // Return
--- public/php/actions/edit/supplier.php
+++ public/php/actions/edit/supplier.php
@@ -3,6 +3,13 @@ include_once('../../functions.php');
 
 $conn = ConnectDB('root', '');
 
+header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Credentials: true');
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST, DELETE, OPTIONS");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 try {
     if (!CheckAuth(3, $conn)) {
         header('Location: ' . $_SERVER['HTTP_REFERER']);
