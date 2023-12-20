30bfc603e57ca02733af1bd399f10fccc0a39667
Baraqqq <iliasbachiri@outlook.com>
2023-12-20 12:48:29 +0100

    Home changes
    Home has been changed

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,78 @@
+import axios from "axios";
 import React from "react";
+import { useState, useEffect } from "react";
+import ReactPaginate from "react-paginate";
 
-export default function home() {
-  return <div>home</div>;
+export default function Customers({ setModal }) {
+  const [data, setData] = useState([]);
+  const [currentItems, setCurrentItems] = useState([]);
+  const [itemOffset, setItemOffset] = useState(0);
+  const [pageCount, setPageCount] = useState(0);
+  const itemsPerPage = 3;
+
+  useEffect(() => {
+    axios
+      .get("http://localhost/code/Vooedselbank-Maaskantje/public/php/json/customerJson.php")
+      .then((res) => {
+        const myArray = Object.keys(res.data).map(key => res.data[key]);
+        console.log(myArray);
+        setData(myArray);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+    setCurrentItems(data.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(data.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage, data]);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % data.length;
+    setItemOffset(newOffset);
+  };
+
+  return (
+    <div className="body-content">
+      <div className="header-content">
+        <h4 className="header-title">Klanten</h4>
+      </div>
+      <table className="data-table">
+        <thead className="table-header">
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>E-mail</th>
+            <th>Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentItems.map((user, index) => {
+            return (
+              <tr key={index}>
+                <td>{user.firstName}</td>
+                <td>{user.lastName}</td>
+                <td>{user.email}</td>
+                <td>{user.phone}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel=" >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={itemsPerPage}
+        pageCount={pageCount}
+        previousLabel="< "
+        containerClassName="pagination"
+        pageLinkClassName="page-num"
+        nextLinkClassName="page-num"
+        previousLinkClassName="page-num"
+        activeClassName="active"
+      />
+    </div>
+    
+  );
 }
