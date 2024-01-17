a30bb196e174fa41fb86bbb864e90cf8f246d44a
Mansoor <2047555@talnet.nl>
2024-01-17 17:45:40 +0100

    created reset password backend

---
+++ public/php/account/database.php
@@ -0,0 +1,19 @@
+<?php
+// connect to database
+$conn = ConnectDB("root", "");
+
+$host = "localhost";
+$dbname = "";
+$username = "root";
+$password = "";
+
+$mysqli = new mysqli(hostname: $host,
+                     username: $username,
+                     password: $password,
+                     database: $dbname);
+                     
+if ($mysqli->connect_errno) {
+    die("Connection error: " . $mysqli->connect_error);
+}
+
+return $mysqli;
