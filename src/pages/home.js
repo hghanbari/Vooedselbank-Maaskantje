7434df8a50ea75d59c84b19507d574ebd7a60e9c
Baraqqq <iliasbachiri@outlook.com>
2024-01-25 13:29:23 +0100

    home.js changes
    2 tabellen naast elkaar.

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,18 +3,68 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 export default function Home() {
-  // const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-  // useEffect(() => {
-  //   axios
-  //     .get("http://localhost/backend/json/customerJson.php")
-  //     .then((res) => {
-  //       const myArray = Object.keys(res.data).map((key) => res.data[key]);
-  //       console.log(myArray);
-  //       setData(myArray);
-  //     })
-  //     .catch((err) => console.log(err));
-  // }, []);
+  useEffect(() => {
+    axios
+      .get("http://localhost/backend/json/customerJson.php")
+      .then((res) => {
+        const myArray = Object.keys(res.data).map((key) => res.data[key]);
+        setData(myArray);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-  return <div className="body-content"></div>;
+  return (
+    <div className="body-content1">
+      <div className="header-content1">
+        <h4 className="header-title1">Klanten</h4>
+      </div>
+      <table className="data-table1">
+        <thead className="table-header1">
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>E-mail</th>
+            <th>Phone</th>
+            <th>Address</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((user, index) => {
+            return (
+              <tr key={index}>
+                <td>{user.firstName}</td>
+                <td>{user.lastName}</td>
+                <td>{user.email}</td>
+                <td>{user.phone}</td>
+                <td>{user.address}</td>
+                <td>
+                  {/* Voeg hier eventueel knoppen toe voor bewerken of verwijderen */}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+
+        <h4 className="header-title1">Leveranciers</h4>
+        <table className="data-table1">
+          <thead className="table-header1">
+            <tr>
+              <th>Naam</th>
+              <th>E-mail</th>
+              {/* Voeg hier eventueel meer kolommen toe */}
+            </tr>
+          </thead>
+          <tbody>
+            {/* Voeg hier gegevens toe voor leveranciers */}
+          </tbody>
+        </table>
+        </div>
+  );
 }
+
+
+
